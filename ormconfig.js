bdc8397d82f272166979d4e2641f676b7f5bbfb9
hugokishi <hugopacheco370@gmail.com>
2021-06-02 16:56:37 -0300

    Add dotenv and config typeorm

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+const rootDir = process.env.ORM_ROOT_DIR;
+
+module.exports = [
+  {
+    name: "default",
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: false,
+    entities: [rootDir + "/domain/entity/**/*.{js,ts}"],
+    migrations: [rootDir + "/domain/migration/*.{js,ts}"],
+    seeds: [rootDir + "/domain/seeds/**/*.{js,ts}"],
+    cli: {
+      entitiesDir: "src/domain/entity",
+      migrationsDir: "src/domain/migration",
+    },
+  },
+]
--- src/interface/http/application.ts
+++ src/interface/http/application.ts
@@ -1,4 +1,7 @@
+import 'reflect-metadata'
+import * as dotenv from 'dotenv'
 import express from 'express'
+import { createConnection } from 'typeorm'
 
 class Application {
   public express: express.Application;
@@ -6,11 +9,17 @@ class Application {
   constructor () {
     this.express = express()
 
+    dotenv.config()
+    this.database()
     this.mountRoutes()
   }
 
   mountRoutes () {
     this.express.get('/health', (_, res) => res.send('App up and running.'))
+  }
+
+  database () {
+    return createConnection()
   }
 }
 
