554fc90efc9ac6e5690d64091a64d00c5ff27f99
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-06-07 12:03:50 -0300

    Add database host env

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,7 @@ module.exports = [
     name: 'default',
     type: 'postgres',
     port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASS,
     database: process.env.DATABASE_NAME,
