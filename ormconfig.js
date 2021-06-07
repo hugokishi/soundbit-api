12530d56a591804c8d23f5eb99e8a0b325b8ee02
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-06-07 08:31:05 -0300

    Lint TypeOrm config file

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,18 @@
-const rootDir = process.env.ORM_ROOT_DIR;
+const rootDir = process.env.ORM_ROOT_DIR
 
 module.exports = [
   {
-    name: "default",
-    type: "postgres",
+    name: 'default',
+    type: 'postgres',
     url: process.env.DATABASE_URL,
     synchronize: false,
     logging: false,
-    entities: [rootDir + "/domain/entity/**/*.{js,ts}"],
-    migrations: [rootDir + "/domain/migration/*.{js,ts}"],
-    seeds: [rootDir + "/domain/seeds/**/*.{js,ts}"],
+    entities: [rootDir + '/domain/entity/**/*.{js,ts}'],
+    migrations: [rootDir + '/domain/migration/*.{js,ts}'],
+    seeds: [rootDir + '/domain/seeds/**/*.{js,ts}'],
     cli: {
-      entitiesDir: "src/domain/entity",
-      migrationsDir: "src/domain/migration",
-    },
-  },
+      entitiesDir: 'src/domain/entity',
+      migrationsDir: 'src/domain/migration'
+    }
+  }
 ]
