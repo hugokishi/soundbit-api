035a978931b9392d8f14aceb28229394770fbb4e
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-06-07 13:07:16 -0300

    Fix User entity

--- src/infrastructure/database/orm/entities/User.ts
+++ src/infrastructure/database/orm/entities/User.ts
@@ -1,37 +1,21 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm'
 
-interface Props {
-  id: string
-  name?: string
-  username: string
-  email: string
-  password: string
-}
-
 @Entity()
 class User {
   @PrimaryColumn()
-  id: string
+  id!: string
 
   @Column({ nullable: true })
   name?: string
 
   @Column()
-  username: string
+  username!: string
 
   @Column()
-  email: string
+  email!: string
 
   @Column()
-  password: string
-
-  constructor ({ id, name, username, email, password }: Props) {
-    this.id = id
-    this.name = name
-    this.username = username
-    this.email = email
-    this.password = password
-  }
+  password!: string
 }
 
 export default User
