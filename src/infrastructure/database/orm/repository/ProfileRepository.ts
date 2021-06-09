e6c5d8e7118d48ebc505b8e05282be1c6fb92421
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-06-09 09:39:23 -0300

    Fix multiple connections issue

--- src/infrastructure/database/orm/repository/ProfileRepository.ts
+++ src/infrastructure/database/orm/repository/ProfileRepository.ts
@@ -1,19 +1,15 @@
-import { Connection as TypeormConn } from 'typeorm'
 import Connection from '../Connection'
 import Profile from '../../../../domain/Profile'
 import ProfileEntityFactory from '../factories/ProfileEntityFactory'
 import ProfileRepository from '../../../../domain/ProfileRepository'
 
 class ProfileRepositoryImpl implements ProfileRepository {
-  readonly connection: Promise<TypeormConn>
-
-  constructor () {
-    this.connection = Connection.connect().then(conn => conn)
-  }
-
   async store (profile: Profile): Promise<Profile> {
-    const profileEntity = ProfileEntityFactory.createProfile(profile);
-    (await this.connection).manager.save(profileEntity)
+    const profileEntity = ProfileEntityFactory.createProfile(profile)
+
+    const connection = await Connection.connect()
+    await connection.manager.save(profileEntity)
+    await connection.close()
 
     return profileEntity
   }
