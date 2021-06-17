72438ab622b1144501633366690f07939f1f3566
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-06-17 19:36:51 -0300

    Implement findByCredentials method
    Add a method on the private repository responsible for searching for profiles that match given credentials

--- src/infrastructure/database/orm/repository/ProfileRepository.ts
+++ src/infrastructure/database/orm/repository/ProfileRepository.ts
@@ -2,6 +2,12 @@ import { getManager } from 'typeorm'
 import Profile from '../../../../domain/Profile'
 import ProfileEntityFactory from '../factories/ProfileEntityFactory'
 import ProfileRepository from '../../../../domain/ProfileRepository'
+import ProfileEntity from '../entities/Profile'
+
+interface Credentials {
+  email: string
+  password: string
+}
 
 class ProfileRepositoryImpl implements ProfileRepository {
   async store (profile: Profile): Promise<Profile> {
@@ -10,6 +16,16 @@ class ProfileRepositoryImpl implements ProfileRepository {
     await getManager().save(profileEntity)
 
     return profileEntity
+  }
+
+  async findByCredentials ({ email, password }: Credentials): Promise<Profile> {
+    const profile = await getManager().findOne(ProfileEntity, { email, password })
+
+    if (!profile) {
+      throw new Error('Profile not found')
+    }
+
+    return new Profile(profile)
   }
 }
 
