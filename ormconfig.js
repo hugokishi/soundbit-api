4e49d22d3974b503aa43fb22e02c944045070d03
hugokishi <hugopacheco370@gmail.com>
2021-06-22 11:24:57 -0300

    Add heroku config

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,13 @@
-const rootDir = process.env.ORM_ROOT_DIR || 'src'
+const rootDir = process.env.ORM_ROOT_DIR
 
-module.exports = [
-  {
-    name: 'default',
-    type: 'postgres',
-    port: process.env.DATABASE_PORT || 5432,
-    host: process.env.DATABASE_HOST || 'localhost',
-    username: process.env.DATABASE_USER || 'postgres',
-    password: process.env.DATABASE_PASS || 'postgres',
-    database: process.env.DATABASE_NAME || 'soundbit_dev',
-    synchronize: false,
-    logging: false,
-    entities: [rootDir + '/infrastructure/database/orm/entities/**/*.{js,ts}'],
-    migrations: [rootDir + '/infrastructure/database/orm/migrations/*.{js,ts}'],
-    seeds: [rootDir + '/infrastructure/database/orm/seeds/**/*.{js,ts}'],
-    cli: {
-      entitiesDir: 'src/infrastructure/database/orm/entities',
-      migrationsDir: 'src/infrastructure/database/orm/migrations'
-    }
+module.exports = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [rootDir + '/infrastructure/database/orm/entities/**/*.{js,ts}'],
+  migrations: [rootDir + '/infrastructure/database/orm/migrations/*.{js,ts}'],
+  seeds: [rootDir + '/infrastructure/database/orm/seeds/**/*.{js,ts}'],
+  cli: {
+    entitiesDir: 'src/infrastructure/database/orm/entities',
+    migrationsDir: 'src/infrastructure/database/orm/migrations'
   }
-]
+}
