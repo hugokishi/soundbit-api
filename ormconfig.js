5bd9a92eff91a509f72e6fcf5c8ce9d5072a16af
hugokishi <hugopacheco370@gmail.com>
2021-06-22 12:03:55 -0300

    refactor typeorm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 const rootDir = process.env.ORM_ROOT_DIR
 
 module.exports = {
-  type: 'sqlite',
-  database: 'soundbit',
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
   entities: [rootDir + '/infrastructure/database/orm/entities/**/*.{js,ts}'],
   migrations: [rootDir + '/infrastructure/database/orm/migrations/*.{js,ts}'],
   seeds: [rootDir + '/infrastructure/database/orm/seeds/**/*.{js,ts}'],
