5f4652bb2c7934f3dcd26f7b0e494991647b37fe
hugokishi <hugopacheco370@gmail.com>
2021-08-05 14:40:32 -0300

    Remove heroku config

--- src/interface/http/server.ts
+++ src/interface/http/server.ts
@@ -1,7 +1,3 @@
 import app from './application'
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`Our app is running on port ${PORT}`)
-})
+app.listen(process.env.PORT || 5000)
